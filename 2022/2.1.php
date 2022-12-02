3626ac599782e62a5dedb1e8284d765ffa877209
Steven Wade <stevenwadejr@gmail.com>
2022-12-02 16:03:05 -0500

    2022 day 2

---
+++ 2022/2.1.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once __DIR__ . '/../InputReader.php';
+$reader = new InputReader(__DIR__ . '/input/2.txt');
+
+enum Choice
+{
+    case ROCK;
+    case PAPER;
+    case SCISSORS;
+
+    public static function elf(string $choice): Choice
+    {
+        return match ($choice) {
+            'A' => Choice::ROCK,
+            'B' => Choice::PAPER,
+            'C' => Choice::SCISSORS,
+        };
+    }
+
+    public static function me(string $choice): Choice
+    {
+        return match ($choice) {
+            'X' => Choice::ROCK,
+            'Y' => Choice::PAPER,
+            'Z' => Choice::SCISSORS,
+        };
+    }
+
+    public function choiceScore(): int
+    {
+        return match ($this) {
+            Choice::ROCK => 1,
+            Choice::PAPER => 2,
+            Choice::SCISSORS => 3,
+        };
+    }
+
+    public function compare(Choice $choice): Outcome
+    {
+        return match (true) {
+            $this === $choice => Outcome::DRAW,
+            $this === Choice::PAPER && $choice === Choice::ROCK => Outcome::WIN,
+            $this === Choice::ROCK && $choice === Choice::SCISSORS => Outcome::WIN,
+            $this === Choice::SCISSORS && $choice === Choice::PAPER => Outcome::WIN,
+            default => Outcome::LOSE,
+        };
+    }
+}
+
+enum Outcome
+{
+    case WIN;
+    case LOSE;
+    case DRAW;
+
+    public function score(): int
+    {
+        return match ($this) {
+            Outcome::WIN => 6,
+            Outcome::LOSE => 0,
+            Outcome::DRAW => 3,
+        };
+    }
+}
+
+$totalScore = 0;
+
+$reader->loop(function ($line) use (&$totalScore) {
+    $choices = explode(' ', $line);
+    $elf = Choice::elf($choices[0]);
+    $me = Choice::me($choices[1]);
+    $outcome = $me->compare($elf);
+
+    $totalScore += $me->choiceScore() + $outcome->score();
+});
+
+echo "Total score: $totalScore\n";
